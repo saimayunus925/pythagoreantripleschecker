b959fe47b26b74111f8b2b7b6438dc2e125833b2
Saima Yunus <saimayunus925@gmail.com>
2019-08-10 01:11:22 -0700

    Update pythagoreantripleschecker.py

--- pythagoreantripleschecker.py
+++ pythagoreantripleschecker.py
@@ -1,19 +1,3 @@
-'''
-
-MAIN GOAL
-
-Create a program that allows the user to input the sides of any triangle, and then return whether the triangle is a Pythagorean Triple 
-or not.
-
-SUBGOALS
-
-If your program requires users to input the sides in a specific order, change the coding so the user can type in the sides in any order.
-Remember, the hypotenuse (c) is always the longest side.
-
-Loop the program so the user can use it more than once without having to restart the program. 
-
-'''
-
 def is_Pythagorean_Triple(a, b, c):
   sum = a**2 + b**2
   if sum == c**2:
